07469c1716775ec835d85ec4dddc88751bc1fc81
snoopyods <snoopyods@gmail.com>
2023-07-26 18:24:39 -0700

    working but barely

--- pokemon_stats.py
+++ pokemon_stats.py
@@ -7,9 +7,10 @@ class Pokemon:
         self.weight=weight
 
     def __str__(self):
-        output = f"----{self.name.title()}----\n"
-        output += f"Height: {self.height}\n"
-        output += f"Weight: {self.weight} \n"
+        print("")
+        output = f"       {self.name.title()}       \n"
+        output += f"       Height: {self.height}       \n"
+        output += f"       Weight: {self.weight}       \n"
         return output
     
     def __repr__(self):
@@ -22,24 +23,32 @@ class PokemonAPI:
 
     
     def __get(self, poke_name):
-
-        res = requests.get(f"{self.base_url}{poke_name}")
+        res = requests.get(f"{self.base_url}/{poke_name}")
         if res.ok:
             return res.json()
-        return
+        return f"{poke_name} isn't a pokemon!"
 
     def get_pokemon(self, pokemon_name):
         data = self.__get(pokemon_name)
         if pokemon_name in data:
-            return f"{pokemon_name} isn't a pokémon!"
+            return f"{pokemon_name.title()} isn't a pokémon!"
         else:
             name = data['name']
             height = data['height']
             weight = data['weight']
             new_pokemon = Pokemon(name,height,weight)
             return new_pokemon
-    
-        
+       
+print("+++=====" *5)        
+
 pokemon1 = PokemonAPI()
-new = pokemon1.get_pokemon('pikapoo')
+new = pokemon1.get_pokemon('squirtle')
 print(new)
+
+print("+++=====" *5)
+
+pokemon2 = PokemonAPI()
+new2 = pokemon2.get_pokemon('squirty')
+print(new2)
+
+print("+++=====" *5)
